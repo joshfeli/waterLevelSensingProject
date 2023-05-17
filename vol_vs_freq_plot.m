42142bdd926dc42b2923bd84569c298129d58ab5
Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>
2023-05-17 12:53:07 -0400

    Updated MATLAB plotting code; attached final report

--- vol_vs_freq_plot.m
+++ vol_vs_freq_plot.m
@@ -6,16 +6,22 @@ meas_freq = [2.2903, 2.33427, 2.3926, 2.47844, 2.6006, 2.802, 3.14537, 3.76388];
 r = 4.125e-2;
 c = 0.3;
 f_111 = 2.27:0.1:4;
-vol2 = pi*r^2*(19.25e-2 - pi*((2*pi*f_111/c).^2-(1.841/(4.125e-2)).^2).^(-.5))*1e3-0.073603;
+vol2 = (pi*r^2*(19.25e-2 - pi*((2*pi*f_111/c).^2-(1.841/(4.125e-2)).^2).^(-.5))*1e3)-0.073603;
+%0.073603
 
-scatter(meas_freq, volume,'filled')
+figure(1)
+scatter(meas_freq, volume, 86, 'filled')
 
 hold on;
 
 plot(f_111, vol2, "r")
 hold off;
 
-title('Frequency vs Volume with offset')
-xlabel('$Frequency (GHz)$','Interpreter','latex','FontSize', 14);
-ylabel('$Volume (L)$','Interpreter','latex','FontSize', 14);
+title('Frequency vs Volume with Offset', 'FontSize', 18)
+xlabel('$\textbf {Frequency (GHz)}$','Interpreter','latex','FontSize', 14);
+ylabel('$\textbf {Volume (L)}$','Interpreter','latex','FontSize', 14);
 leg = legend('Measured', 'Expected','Location', 'southeast');
+
+%once this is run, in command window, type improvePlot and hit Enter;
+%then type ylim([0 0.8]) and hit Enter
+%note: you must put improvePlot.m and this file in the same folder
